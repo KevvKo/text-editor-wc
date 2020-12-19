69f35329119447ad07ba0a0161cc7f1ff29c642e
KevvKo <kklein915@gmail.com>
2020-12-19 19:45:38 +0100

    add index.html

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-class TextEditor{
+class TextEditor extends HTMLElement    {
 
     constructor(){
-        super()
+        super();
+
+        const template = document.createElement("template");
+        template.id = "text-editor";
+
+        this.shadowRoot = this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+
     }
 }
 
