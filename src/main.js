004610ee464824efb383efc71ce4f58ed77efc51
KevvKo <kklein915@gmail.com>
2020-12-20 02:01:43 +0100

    setup first configs for webpack

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+
+class TextEditor extends HTMLElement    {
+
+    constructor(){
+        super();
+
+        const template = document.createElement("template");
+        template.id = "text-editor";
+
+        this.shadowRoot = this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+
+    }
+}
+
+customElements.define('text-editor', TextEditor)
