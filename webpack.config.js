bee684a2a57b6dede523d3f04d6f59a054c6d0f0
KevvKo <kklein915@gmail.com>
2020-12-20 17:10:09 +0100

    config webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
+module.exports = ({mode}) => {
+
+  return {
+    mode,
     entry: './src/main.js',
+    devtool: 'inline-source-map',
+    devServer: {
+      open: true,
+      contentBase: './src',
+      compress: true,
+      port: 3030
+    },
+    plugins: [
+
+      new HtmlWebpackPlugin({
+        title: 'Text-Editor-WC',
+      }),
+    ],
     output: {
       filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-  };
+      path: path.resolve(__dirname, 'src'),
+    }
+  }
+};
