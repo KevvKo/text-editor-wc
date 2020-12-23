d4a10711c0654244143ac41dcc9e52d499911c97
KevvKo <kklein915@gmail.com>
2020-12-23 11:48:25 +0100

    update icons and insert in template

--- src/main.js
+++ src/main.js
@@ -27,6 +27,11 @@ class TextEditor extends HTMLElement {
         </style>
         <div id="text-editor" >
             <div id="toolbox">
+                <img src="./assets/img/format_bold-black-24dp.svg">
+                <img src="./assets/img/format_italic-black-24dp.svg">
+                <img src="./assets/img/format_underlined-black-24dp.svg">
+                <img src="./assets/img/format_list_bulleted-black-24dp.svg">
+                <img src="./assets/img/format_list_numbered-black-24dp.svg">
             </div>
             <div id="content" contenteditable="true"></div>
         </div>`;
