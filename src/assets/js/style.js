6d2e62c7862acc43b8c6878aded3b84d2d9e0663
KevvKo <kklein915@gmail.com>
2021-01-02 23:21:31 +0100

    update border colors

--- src/assets/js/style.js
+++ src/assets/js/style.js
@@ -6,7 +6,7 @@ export const style = `
     display: flex;
     flex-direction: column;
     border-radius: 5px;
-    border: 1px solid #D2D2D2; 
+    border: 1px solid #A8A8A8; 
 }
 
 .toolbox-button{
@@ -17,7 +17,7 @@ export const style = `
 }
 
 .active{
-    background: #DADADA;
+    background: #A8A8A8;
 }
 
 .active img{
@@ -27,18 +27,18 @@ export const style = `
 .toolbox-button:hover{
     cursor: pointer;
     transition: 0.1s;
-    background: #DADADA;
+    background: #A8A8A8;
 }
 
 #underlined{
     padding-right: 4px;
     margin-right: 0;
-    border-right: 1px solid #DADADA;
+    border-right: 1px solid #A8A8A8;
 }
 
 #content{ 
     border: none;
-    border-top: 1px solid #D2D2D2; 
+    border-top: 1px solid #A8A8A8; 
     padding: 10px;
     flex-grow: 2; 
     word-break: break-all;
